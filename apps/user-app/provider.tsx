edcb089e008bd6f825e46209d6e3411942fa5945
walianitin <walianitin828@gmail.com>
2025-06-06 15:26:32 +0530

    asdf

--- apps/user-app/provider.tsx
+++ apps/user-app/provider.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
-
+// This file is used to provide global state management and session handling for the application.
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return <RecoilRoot>
         <SessionProvider>
             {children}
         </SessionProvider>
     </RecoilRoot>
-}
+} 
